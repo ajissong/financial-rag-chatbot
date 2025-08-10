369bb70dffe2369a451c72417ac6085d2b4b2256
ajissong <song.eunji@gmail.com>
2025-08-10 07:24:42 +0000

    try bench

--- finrag.py
+++ finrag.py
@@ -1,7 +1,6 @@
 import os
 from langchain_openai import ChatOpenAI
 from langchain_openai import OpenAIEmbeddings
-# from langchain_community.vectorstores import Chroma
 from langchain_chroma import Chroma
 from langchain_community.document_loaders import PyPDFLoader
 from langchain.chains import RetrievalQA
@@ -16,23 +15,26 @@ import gradio as gr
 from dotenv import load_dotenv
 load_dotenv()
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+OPENAI_MODEL_NAME = os.getenv("OPENAI_MODEL_NAME")
+DB_NAME = os.getenv("DB_NAME")
+PDF_FILE_NAME = os.getenv("PDF_FILE_NAME")
 
 def load_pdf():
     # urllib.request.urlretrieve("https://github.com/chatgpt-kr/openai-api-tutorial/raw/main/ch07/2020_%EA%B2%BD%EC%A0%9C%EA%B8%88%EC%9C%B5%EC%9A%A9%EC%96%B4%20700%EC%84%A0_%EA%B2%8C%EC%8B%9C.pdf", filename="2020_경제금융용어 700선_게시.pdf")
 
-    loader = PyPDFLoader("2020_경제금융용어 700선_게시.pdf")
+    loader = PyPDFLoader(PDF_FILE_NAME)
     texts = loader.load_and_split()
-    texts = texts[13:-1]
+    # texts = texts[13:-1]
 
     return texts
 
 def create_vectordb():
     # 기존 DB가 있는지 확인
-    if os.path.exists("./chroma_db"):
+    if os.path.exists(f"./{DB_NAME}"):
         print("기존 벡터 DB를 로드합니다...")
         embedding = OpenAIEmbeddings(chunk_size=100)
         vectordb = Chroma(
-            persist_directory="./chroma_db",
+            persist_directory=f"./{DB_NAME}",
             embedding_function=embedding
         )
         print(f"로드된 문서 수: {vectordb._collection.count()}")
@@ -46,7 +48,7 @@ def create_vectordb():
     vectordb = Chroma.from_documents(
         documents=texts,
         embedding=embedding,
-        persist_directory="./chroma_db"  # DB를 현재 폴더에 저장
+        persist_directory=f"./{DB_NAME}"  # DB를 현재 폴더에 저장
     )
     
     # DB를 디스크에 저장 (Chroma 0.4.x에서는 자동으로 저장됨)
@@ -62,19 +64,25 @@ def create_retriever():
 
 def create_chain():
 
-    template = """당신은 ABC은행에서 만든 금융 용어를 설명해주는 금융쟁이입니다.
-    주어진 검색 결과를 바탕으로 답변하세요.
-    검색 결과에 없는 내용이라면 답변할 수 없다고 하세요. 반말로 친근하게 답변하세요.
+    # template = """당신은 ABC은행에서 만든 금융 용어를 설명해주는 금융쟁이입니다.
+    # 주어진 검색 결과를 바탕으로 답변하세요.
+    # 검색 결과에 없는 내용이라면 답변할 수 없다고 하세요. 반말로 친근하게 답변하세요.
     
-    Context: {context}
+    # Context: {context}
 
+    # Question: {question}
+    # Answer:
+    # """
+
+    template = """You are a helpful assistant that can answer questions about the given context.
+    Context: {context}
     Question: {question}
     Answer:
     """
 
     retriever = create_retriever()  # 기존 retriever 재사용
     prompt = PromptTemplate.from_template(template)
-    llm = ChatOpenAI(model_name="gpt-4o-mini", temperature=0)
+    llm = ChatOpenAI(model_name=OPENAI_MODEL_NAME, temperature=0)
 
     # retriever 결과를 문자열로 변환하는 함수
     def format_docs(docs):
@@ -122,9 +130,14 @@ def create_ui(chain):
     # 인터페이스 실행
     demo.launch(debug=True)
 
-def main():
-    chain = create_chain()
+def benchmark(chain):
+    from datasets import load_dataset
 
+    ds = load_dataset("TheFinAI/en-acronym", token=os.getenv("HF_TOKEN"))
+
+    print(ds)
+
+def main():
     questions = ["디커플링이란 무엇인가?", "너는 뭘하는 챗봇이니?"]
 
     results = chain.batch([{"question":q} for q in questions])
@@ -132,14 +145,8 @@ def main():
         print(f"\n질문: {q}")
         print(f"답변: {r}")
 
-    # for question in questions:
-    #     print(f"\n질문: {question}")
-    #     try:
-    #         result = chain.invoke({"question": question})
-    #         print(f"답변: {result}")
-    #     except Exception as e:
-    #         print(f"에러 발생: {e}")
-
 if __name__ == "__main__":
-    # main()
-    create_ui(create_chain())
+    chain = create_chain()
+    # main(chain)
+    # create_ui(chain)
+    benchmark(chain)
